62719674081b35e04eb07ba02127f3941a35f94f
lizongji <tidemedia@tidemedia.local>
2017-10-11 11:56:46 +0800

    创建应用

---
+++ newItemForiOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  newItemForiOS
+//
+//  Created by tidemedia on 2017/10/11.
+//  Copyright © 2017年 tidemedia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ newItemForiOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  newItemForiOS
+//
+//  Created by tidemedia on 2017/10/11.
+//  Copyright © 2017年 tidemedia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
